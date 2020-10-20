ae4a156575b36817dc3436bd7cefd3d4c2e786ba
fadlifauzi123 <71370769+fadlifauzi123@users.noreply.github.com>
2020-10-20 13:24:03 +0700

    Add files via upload

---
+++ pbo-reg2 tugasPertemuan5/src/com/pboreg/Pelajar.java
@@ -0,0 +1,27 @@
+package com.pboreg;
+
+class Pelajar {
+    String nama= "Fadli Fauzi";
+    String NIM= "201971036";
+    String jurusan= "Informatika";
+    double ipk= 2.64;
+    int umur= 18;
+
+    public String tampilkanNama(){
+        return this.nama;
+}
+    public String tampilkanNIM(){
+        return this.NIM;
+    }
+    public String tampilkanJurusan(){
+        return this.jurusan;
+    }
+    public double tampilkanIPK(){
+        return this.ipk;
+    }
+    public int tampilkanUmur(){
+        return this.umur;
+    }
+
+}
+
